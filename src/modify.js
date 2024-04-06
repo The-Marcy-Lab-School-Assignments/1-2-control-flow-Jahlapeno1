2d52404f1fef6edd403ae372b96d209a9313fcbb
Jahlapeno1 <jimenezaot@gmail.com>
2024-04-05 23:25:57 -0400

    Answered q6

--- src/modify.js
+++ src/modify.js
@@ -1,5 +1,8 @@
 const wildlyBiasedReview = (location) => {
-  if (location === 'NYC') {
+  if (typeof location !== 'string') {
+    return 'Guard clause'
+  }
+  else if (location === 'NYC') {
     console.log('THE GREATEST CITY IN THE WORLD');
     console.log('THE CITY THAT NEVER SLEEPS');
     console.log('WOW WHAT A CITY');
